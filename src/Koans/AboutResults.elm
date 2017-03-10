62edde0ca2a7f0d8d7dc63bb7b2607e38c7cf842
dilabuser <dilabuser@havoc-5.local>
2017-03-10 08:27:00 +0100

    about results

--- src/Koans/AboutResults.elm
+++ src/Koans/AboutResults.elm
@@ -9,26 +9,26 @@ testSuite =
     describe "About Result"
         [ test "results represent the result of a function" <|
             \() ->
-                xResult
+                (Ok 42)
                     |> Expect.equal (Ok 42)
         , test "but could be an error" <|
             \() ->
-                xResult
+                (Err "there was an error")
                     |> Expect.equal (Err "there was an error")
         , test "a result can be converted to a maybe" <|
             \() ->
-                xMaybe
+                (Just 42)
                     |> Expect.equal (Result.toMaybe (Ok 42))
         , test "but an error will become nothing" <|
             \() ->
-                xMaybe
+                Nothing
                     |> Expect.equal (Result.toMaybe (Err "there was an error"))
         , test "a maybe can also be converted to a result" <|
             \() ->
-                xResult
+                (Ok 42)
                     |> Expect.equal (Result.fromMaybe "there was an error" (Just 42))
         , test "and will become an error if there is nothing" <|
             \() ->
-                xResult
+                (Err "there was an error")
                     |> Expect.equal (Result.fromMaybe "there was an error" Nothing)
         ]
